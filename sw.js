af8a8cbf65ce787cf860561c38e15ec5d015aa9c
tbone278 <tbone278@googlemail.com>
2026-06-06 12:11:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-
-const CACHE='airportfinder-v12';
-const FILES=['./','./index.html','./app.js','./style.css','./manifest.json','./airports.json'];
-
-self.addEventListener('install',e=>{
- e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
-});
-
-self.addEventListener('fetch',e=>{
- e.respondWith(
-  caches.match(e.request).then(r=>r || fetch(e.request))
- );
-});
+const CACHE='airportfinder-v13';self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.json'])));});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
